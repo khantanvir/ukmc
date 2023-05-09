7e9de6e2a80b9fdc0d69d866ef158edc4f093ad8
khantanvir <aiub.tanvir@gmail.com>
2023-05-09 20:40:22 +0600

    work on auth panel
    Signed-off-by: khantanvir <aiub.tanvir@gmail.com>

--- resources/views/adminpanel.blade.php
+++ resources/views/adminpanel.blade.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
     <title>{{ (!empty($page_title))?$page_title:'' }}</title>
     <link rel="icon" type="image/x-icon" href="{{ asset('backend/src/assets/img/favicon.ico') }}" />
     <link href="{{ asset('backend/layouts/vertical-dark-menu/css/light/loader.css') }}" rel="stylesheet" type="text/css" />
@@ -272,12 +273,86 @@
     <script src="{{ asset('backend/src/assets/js/dashboard/dash_1.js') }}"></script>
     <script src="{{ asset('backend/src/assets/js/dashboard/dash_2.js') }}"></script>
     <!-- BEGIN PAGE LEVEL PLUGINS/CUSTOM SCRIPTS -->
-
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
      <!-- BEGIN PAGE LEVEL SCRIPTS -->
      <script src="{{ asset('backend/src/plugins/src/stepper/bsStepper.min.js') }}"></script>
      <script src="{{ asset('backend/src/plugins/src/stepper/custom-bsStepper.min.js') }}"></script>
      <!-- END PAGE LEVEL SCRIPTS -->
+     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+	 <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+     <script>
+		@if(Session::has('success'))
+		toastr.options =
+		{
+			"closeButton" : true,
+			"progressBar" : true,
+			"timeOut": "10000",
+			"extendedTimeOut": "1000",
+			"showEasing": "swing",
+			"hideEasing": "linear",
+		}
+				toastr.success("{{ session('success') }}");
+		@endif
 
+		@if(Session::has('error'))
+		toastr.options =
+		{
+			"closeButton" : true,
+			"progressBar" : true,
+			"timeOut": "10000",
+			"positionClass": "toast-top-right",
+			"extendedTimeOut": "1000",
+			"showEasing": "swing",
+			"hideEasing": "linear",
+		}
+				toastr.error("{{ session('error') }}");
+		@endif
+
+		@if(Session::has('info'))
+		toastr.options =
+		{
+			"closeButton" : true,
+			"progressBar" : true,
+			"timeOut": "10000",
+			"extendedTimeOut": "1000",
+			"showEasing": "swing",
+			"hideEasing": "linear",
+		}
+				toastr.info("{{ session('info') }}");
+		@endif
+
+		@if(Session::has('warning'))
+		toastr.options =
+		{
+			"closeButton" : true,
+			"progressBar" : true,
+			"timeOut": "10000",
+			"extendedTimeOut": "1000",
+			"showEasing": "swing",
+			"hideEasing": "linear",
+		}
+				toastr.warning("{{ session('warning') }}");
+		@endif
+	  </script>
+	  <script type="text/javascript">
+		$.ajaxSetup({
+			headers: {
+				'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+			}
+		});
+		</script>
+        <script>
+            $(document).ready(function(){
+                getAttributeData();
+            });
+        </script>
+        <script>
+            function getAttributeData(){
+				$.get("https://staging-api.theleadlibrary.com/api/v1/get-country-list",function(data,status){
+					console.log(data);
+				});
+			}
+        </script>
 
 
 </body>
